6359222a59a3a9c3ef8e2ca21cb9658ca7dd1ab7
AlonEfrati <alonefrati@hotmail.com>
2026-03-11 22:05:21 +0100

    deploy proxy

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.proxies import WebshareProxyConfig
 
 app = FastAPI()
 
 
 @app.get("/transcript/{video_id}")
 def get_transcript(video_id: str):
-    ytt_api = YouTubeTranscriptApi()
+    proxy_config = WebshareProxyConfig(
+        proxy_username="ortdpewv-1",
+        proxy_password="4wsz5w2utihy",
+    )
+    ytt_api = YouTubeTranscriptApi(proxy_config=proxy_config)
     result = ytt_api.fetch(video_id)
     return {"transcript": result}
