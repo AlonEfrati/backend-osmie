55b64e2df918eaabae972ddb867bf03b99c867c9
AlonEfrati <alonefrati@hotmail.com>
2026-04-17 19:46:39 +0200

    update proxy username

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ app = FastAPI()
 @app.get("/transcript/{video_id}")
 def get_transcript(video_id: str):
     proxy_config = WebshareProxyConfig(
-        proxy_username="ortdpewv-1",
+        proxy_username="ortdpewv-rotate",
         proxy_password="4wsz5w2utihy",
+        domain_name="proxy.webshare.io",
+        port=80,
     )
     ytt_api = YouTubeTranscriptApi(proxy_config=proxy_config)
     result = ytt_api.fetch(video_id)
