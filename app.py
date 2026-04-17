8656f1136dd6790bce04e1d82e830da3a96120db
AlonEfrati <alonefrati@hotmail.com>
2026-04-17 19:53:40 +0200

    update prot

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ def get_transcript(video_id: str):
     proxy_config = WebshareProxyConfig(
         proxy_username="ortdpewv-rotate",
         proxy_password="4wsz5w2utihy",
-        domain_name="proxy.webshare.io",
-        port=80,
     )
     ytt_api = YouTubeTranscriptApi(proxy_config=proxy_config)
     result = ytt_api.fetch(video_id)
